a4208243bd8f631e623279de3a1be602446bd603
shanmu1601 <saiprabhasukumar19@gmail.com>
2020-07-24 19:48:23 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import './game-main.dart';
+
+void main() {
+  runApp(MyApp());
+  //Force Screen to Landscape
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Game Box Jumping',
+      home: GameMain(),
+    );
+  }
+}
---
+++ lib/settings/game-setting.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const double mainWidth = double.infinity,
+    mainHeight = 150.0,
+    initSpawnObstacle = 60.0,
+    initGameSpeed = 13.0,
+    gravity = 1.0;
+
+const Color backgroundMaincolor = Colors.grey;
+
+double gameSpeed = initGameSpeed, spawnObstacle = initSpawnObstacle;
